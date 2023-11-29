9a745b4de523bdc61ce51f135cf67201185a758c
Meszarosistvan0828 <84836386+Meszarosistvan0828@users.noreply.github.com>
2023-11-29 21:56:31 +0100

    mégtöbb dizájnolás vége fele vagyunk vagy mégse ezt a kedves hallgató döntse el magában avrage fankadeli enjjoyer

--- main.js
+++ main.js
@@ -181,7 +181,7 @@ idozito2=setInterval(() => {
     idozito = setInterval(()=>{
         let poz = Math.floor(Math.random()*szamsor.length);
         nyeremeny = szamsor[poz];
-        document.getElementById("slot").innerHTML = nyeremeny;
+        document.getElementById("slot").innerHTML = '<img src=porgetszam/'+nyeremeny+'.png>';
         meddigmenjen--;
         console.log(meddigmenjen)
 
@@ -191,7 +191,7 @@ idozito2=setInterval(() => {
            
         }
        
-    },75)
+    },100)
 }
 
 
